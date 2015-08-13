464c5f299b50a54620ab6edc95a7f5df7ff443c5
unknown <johnpeng47@gmail.com>
2015-08-12 21:53:00 -0400

    changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node read.js
--- read.js
+++ read.js
@@ -1,11 +1,16 @@
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-MongoClient.connect('mongodb://localhost:27017/database1',function())
-	db.users.find(function(err,results){
-		if(err){
-			console.log('Works but error: '+err);
-		}
-		else{
-			console.log(results);
-		}
-	})
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// views is directory for all template files
+
+app.get('/', function(request, response) {
+  response.send('THIS IS MY HEROKU APP and WEST INDIE MUSIC SUCKS');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+

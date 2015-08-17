a907cd1400e3f7e20bab1ed3d2d1d535800949e3
unknown <johnpeng47@gmail.com>
2015-08-16 22:22:03 -0400

    socketio: server echo client

--- mongo-connect.js
+++ mongo-connect.js
@@ -1,15 +1,25 @@
+//server
 var express = require('express');
-var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//routes
 var insert_routes = require('./routes/index.js');
 var generate_random = require('./routes/randomdata.js');
 var port = process.env.PORT || 8080
+var bodyParser = require('body-parser');
 // var shutdown = require('./routes/close-connection.js');
 // var remove = require('./routes/delete.js');
 // var query = require('./query.js');
 // //express middleware parses incoming post requests and parses the json body; 
 //returned output is stored in req.body, and is sent off to the next middleware
 var Users = [];
+app.use('/views',express.static(path.join(__dirname, 'html')));
+
+app.get('/interface',function(req,res){
+  res.render(path.join(__dirname,'html','index.html'));
+})
 app.get('/',function(req,res){
 	res.send('HELLO THERE');
 	console.log('I HIT HEROKU');
@@ -21,6 +31,17 @@ app.get('/print',function(req,res){
 		console.log(Users[i]);
 	}
 })
+//socket.io application
+io.on('connection',function(socket){
+  console.log('user has connected');
+  socket.on('disconnect',function(){
+    console.log('client disconnected..');
+  })
+  socket.on('chatter',function(msg){
+    console.log('message: '+msg);
+  })
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -38,6 +59,6 @@ app.post('/recieve',function(req,res){
 	console.log(req.body);
 	res.send(req.body);
 })
-app.listen(port,function(){
+http.listen(port,function(){
 	console.log('im listening on port: '+port);
 })
---
+++ routes/query.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(Users){
+
+	for(i=0;i<Users.length;i++){
+		if( )
+	}
+}

f52f9288abbb2a16f2ed77fccc0c796be1a83c75
unknown <pengj4@TDL00880931.TDBFG.com>
2015-08-27 11:51:03 -0400

    refactor plus added reconnect

--- mongo-connect.js
+++ mongo-connect.js
@@ -12,38 +12,12 @@ var bodyParser = require('body-parser');
 // var shutdown = require('./routes/close-connection.js');
 // var remove = require('./routes/delete.js');
 var query = require('./query.js');
-var Users = [];
+var User = null;
 var matched = [];
 var room_number = 0;
-//query function
-// for(i=0;i<10;i++){
-// 	console.log('im in 20');
-// 	var input = {
-// 		id:i,
-// 		timesubmitted:String(Math.random()).split('0.')[1]
-// 	}
-// 	Users.push(input);
-// }
-// setInterval(function(){
-// 	console.log('hello');
-// 	if(Users.length<=1){
-// 		console.log('empty array');
-// 	}
-// 	else{
-// 		var room = [Users[0],Users[1]];
-// 		Users.shift();
-// 		Users.shift();
-// 		console.log("Users: ");
-// 		console.log(Users);
-// 		console.log("Room: ");
-// 		console.log(room);
-// 	}
-// },1000);
-
-
-// //express middleware parses incoming post requests and parses the json body; 
-//returned output is stored in req.body, and is sent off to the next middleware
-
+//functions...
+//checks if socket is in disconnect array and reconnects to its original room
+///
 app.use('/views',express.static(path.join(__dirname, 'html')));
 
 app.get('/rooms',function(req,res){
@@ -51,72 +25,101 @@ app.get('/rooms',function(req,res){
 	res.end();
 })
 
-app.get('/interface',function(req,res){
-  res.render(path.join(__dirname,'html','index.html'));
-})
-app.get('/',function(req,res){
-	res.send('HELLO THERE');
-	console.log('I HIT HEROKU');
-})
 app.get('/print',function(req,res){
-	res.send('im in here2');
-	console.log('User list: ');
-	for(i=0;i<Users.length;i++){
-		console.log(Users[i]);
-		res.send(Users[i]);
-	}
+	res.send(matched);
+	res.end();
 })
 //socket.io application
 io.on('connection',function(socket){
-	console.log('user '+socket.id+' has connected');
-	var user = {
-  		socketid:socket.id,
-  		timesubmitted:new Date().getTime()
-	}
-	Users.push(user);
-	if(Users.length%2==0){
-		console.log('emitting rooms....');
-		var roomnumber = room_number+1;
-		var index1 = Users.length-1;
-		var index2 = Users.length-2;
-		console.log(Users);
-		var socketids ={
-			socket1:Users[index1].socketid,
-			socket2:Users[index2].socketid,
-			room:String('room'+roomnumber)
+	socket.on('check-in',function(ident){
+		console.log(ident);
+		//should execute on connections after first time
+		if(ident!=null){
+			console.log('ident recieved');
+			console.log(ident);
+			for(i=0;i<matched.length;i++){
+				if(ident==matched[i].id1){
+					socket.join(matched[i].room);
+					matched[i].id1 = socket.id;
+					socket.emit('cache_client',socket.id);
+					console.log('client reconnected');
+				}
+				if(ident==matched[i].id2){
+					socket.join(matched[i].room);
+					matched[i].id2 = socket.id;
+					socket.emit('cache_client',socket.id);
+					console.log('client reconnected');
+				}
+			}
 		}
-		io.sockets.emit('establish connection',socketids);
-		room_number++;
-	}
-	socket.on('disconnect',function(){
-	    console.log('client disconnected..');
+		//should only execute on first time connections
+		else{
+			console.log('user '+socket.id+' has connected');
+			if(User!=null){
+				var room = 'room'+room_number;
+				//puts current socket id in localstorage on client side
+				socket.emit('cache_client',socket.id);
+				socket.join(room);
+				User.join(room);
+				var input = {
+					id1:User.id,
+					id2:socket.id,
+					room: room
+				}
+				matched.push(input);
+				console.log('user '+socket.id+' and user '+User.id+' has joined the room');
+				User=null;
+			}
+			else{
+				socket.emit('cache_client',socket.id);
+				User=socket;
+			}
+		}
 	})
- 	//demo testing messaging
-	// socket.on('chatter',function(msg){
-	//     console.log('message: '+msg);
-	//     socket.emit('chat message',msg);
-	// })
-	//echo messages to all private rooms
-	socket.on('connectme',function(room){
-		console.log(socket.id+" has joined room: "+room);
-		socket.join(room);
-		socket.on('chatter',function(msg){
-			console.log(socket.id+": "+msg);
-			io.to(room).emit('chatmsg',msg);
-		})
-
+	socket.on('chatter',function(msg){
+		console.log('chatter has been recieved');
+		for(i=0;i<matched.length;i++){
+			if((socket.id==matched[i].id1)||(socket.id==matched[i].id2)){
+				console.log('chat msg sent out');
+				io.to(matched[i].room).emit('chatmsg',msg);
+			}
+		}
+	})
+	socket.on('disconnect',function(){
+		console.log('client '+socket.id+" has disconnected");
+		// for(i=0;i<matched.length;i++){
+		// 	if(matched[i].id1==socket.id){
+		// 		socket.emit('cache_client',matched[i].id1);
+		// 		console.log('socket id '+matched[i].id1+' has been sent to client');
+		// 	}
+		// 	if(matched[i].id2==socket.id){
+		// 		socket.emit('cache_client',matched[i].id2);
+		// 		console.log('socket id '+matched[i].id2+' has been sent to client');
+		// 	}
+		// }
+	})
+	socket.on('checking',function(id){
+		console.log('i am :');
+		console.log(id);
+	})
+	app.get('/clear',function(req,res){
+		io.sockets.emit('clear-storage');
+		res.end();
+	})
+	app.get('/getUserID',function(req,res){
+		io.sockets.emit('print-storage');
+		res.end();
 	})
 })
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 // app.use('/query',query(Users));
-app.use('/insert',insert_routes(Users));
-app.use('/random',generate_random(Users));
-app.use('/query',query(Users));
+// app.use('/insert',insert_routes(Users));
+// app.use('/random',generate_random(Users));
+// app.use('/query',query(Users));
 
 
 // app.use('/shutdown',shutdown);
